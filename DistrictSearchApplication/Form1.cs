ebaea34a37894772287a53e554be0d21ee9e5125
Akram <akramewu@gmail.com>
2015-11-15 22:35:09 +0100

    District Search Full Application

--- DistrictSearchApplication/Form1.cs
+++ DistrictSearchApplication/Form1.cs
@@ -39,30 +39,8 @@ namespace DistrictSearchApplication
         {
             var query = textBox1.Text.ToUpper();
             List<string> result = new List<string>();
-            //foreach (string district in districts)
-            //{
-            //catch the input of the textbox and store it in a variable
-            //string input = textBox1.Text;
-
-            //check the variable with the temporary value of the districts(district)
-            //if (district.StartsWith(input))
-            {
-                //add this temporary variable(district) into the result 
-
-                //   result.Add(district);
-                // }
-
-                //}
-                //there should be a plural(list,array,collection.....)
-                //there should be a logic check of boolean(method,operator check,multiple operator,multiple method)
-                //select appropriate data into a new list
-
-                //from a variable in a plural
-                //where theConditionIsTrue
-                //select theVariable
-
                 result = (from d in districts
-                    where d.ToUpper().StartsWith(query)
+                    where d.ToUpper().Contains(query)
                     select d).ToList();
 
                 //reload, re datasource to the listbox
